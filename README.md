575ffd92364162eea17fd90d2f464fd19af50b91
myhsia <myhsia@outlook.com>
2026-01-15 16:16:51 +0800

    New physics3 package initialed

---
+++ README.md
@@ -0,0 +1,46 @@
+[![CTAN Version](https://img.shields.io/ctan/v/physics3)](https://ctan.org/pkg/physics3)
+[![GitHub Last Commit](https://img.shields.io/github/last-commit/myhsia/physics3)](https://github.com/myhsia/physics3/commits)
+[![GitHub Repo stars](https://img.shields.io/github/stars/myhsia/physics3)](https://github.com/myhsia/physics3)
+
+The `physics3` Package
+======================
+
+This package defines commands for typesetting math formulae faster and
+more simply. `physics3` is a modularized package, each module provides its
+own function while `physics3.sty` itself only provides very few functions.
+Therefore you can load modules separately after loading `physics3`.
+
+Modules of `physics3` provide the following supports:
+
+- Automatic braces;
+- Dirac bra-ket notation;
+- Easy way to typeset diagonal matrices and matrices with similar entries;
+- Double cross and double dot (binary) operators for tensors;
+
+Issues
+------
+
+The issue tracker for `LXGW` is currently located
+[on GitHub](https://github.com/myhsia/physics3).
+
+Copyright and License
+---------------------
+
+  Copyright (C) 2026      by Mingyu Xia     <myhsia@outlook.com>
+
+  Copyright (C) 2022-2024 by Tingxuan Zhang <alphaztx@163.com>
+
+This work may be distributed and/or modified under the conditions
+of the LaTeX Project Public License (LPPL), either version 1.3c of
+this license or (at your option) any later version.
+The latest version of this license is in
+
+    http://www.latex-project.org/lppl.txt
+
+and version 1.3c or later is part of all distributions of LaTeX
+version 2008 or later.
+
+This work has the LPPL maintenance status `maintained'.
+
+The Current Maintainer of this work is **Mingyu Xia**.
+
---
+++ build.lua
@@ -0,0 +1,87 @@
+--[==========================================[--
+            L3BUILD FILE FOR PHYSICS3
+      Once Pushed With This File Modified
+        A New Release Will Be Published
+--]==========================================]--
+
+--[==========================================[--
+               Basic Information
+             Do Check Before Push
+--]==========================================]--
+
+module              = "physics3"
+version             = "v0.1A"
+date                = "2026-01-17"
+maintainer          = "Mingyu Xia"
+uploader            = "Mingyu Xia"
+maintainid          = "myhsia"
+email               = "myhsia@outlook.com"
+repository          = "https://github.com/" .. maintainid .. "/" .. module
+announcement        = ""
+summary             = ""
+description         = ""
+
+--[==========================================[--
+          Build, Pack, Tag, and Upload
+         Do not Modify Unless Necessary
+--]==========================================]--
+
+ctanzip             = module
+cleanfiles          = {"*.log", "*.pdf", "*.zip", "*.curlopt"}
+excludefiles        = {"*~"}
+textfiles           = {"*.md", "LICENSE", "*.lua"}
+typesetexe          = "latexmk -xelatex -shell-escape"
+typesetruns         = 1
+uploadconfig  = {
+  pkg          = module,
+  version      = version .. " " .. date,
+  author       = maintainer,
+  uploader     = uploader,
+  email        = email,
+  summary      = summary,
+  description  = description,
+  license      = "lppl1.3c",  
+  ctanPath     = "/macros/latex/contrib/" .. module,
+  announcement = announcement,
+  home         = "https://github.com/" .. maintainid,
+  bugtracker   = repository .. "/issues",
+  support      = repository .. "/issues",
+  repository   = repository,
+  development  = "https://github.com/" .. maintainid,
+  update       = true
+}
+function update_tag(file, content, tagname, tagdate)
+  tagname = version
+  tagdate = date
+  if string.match(file, "%.dtx$") then
+    content = string.gsub(content,
+      "\\ProvidesExplPackage {" .. module .. "} %{[^}]+%} %{[^}]+%}[\r\n%s]*%{[^}]+%}",
+      "\\ProvidesExplPackage {" .. module .. "} {" .. tagdate .. "} {" .. tagname .. "}\n  {" .. summary .. "}")
+    content = string.gsub(content,
+      "\\date{Released %d+%-%d+%-%d+\\quad \\texttt{v([%d%.A-Z]+)}}",
+      "\\date{Released " .. tagdate .. "\\quad \\texttt{" .. tagname .. "}}")
+  end
+  return content
+end
+
+--[== "Hacks" to `l3build` | Do not Modify ==]--
+
+function docinit_hook()
+  cp(ctanreadme, unpackdir, currentdir)
+  return 0
+end
+function tex(file,dir,cmd)
+  dir = dir or "."
+  cmd = cmd or typesetexe
+  if os.getenv("WINDIR") ~= nil or os.getenv("COMSPEC") ~= nil then
+    upretex_aux = "-usepretex=\"" .. typesetcmds .. "\""
+    makeidx_aux = "-e \"$makeindex=q/makeindex -s " .. indexstyle .. " %O %S/\""
+    sandbox_aux = "set \"TEXINPUTS=../unpacked;%TEXINPUTS%;\" &&"
+  else
+    upretex_aux = "-usepretex=\'" .. typesetcmds .. "\'"
+    makeidx_aux = "-e \'$makeindex=q/makeindex -s " .. indexstyle .. " %O %S/\'"
+    sandbox_aux = "TEXINPUTS=\"../unpacked:$(kpsewhich -var-value=TEXINPUTS):\""
+  end
+  return run(dir, sandbox_aux .. " " .. cmd         .. " " ..
+                  upretex_aux .. " " .. makeidx_aux .. " " .. file)
+end
